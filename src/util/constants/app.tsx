3c0cf5369b7f0964d66322d1aa920f0da678d8ac
Park-junseo <psbsanno@gmail.com>
2025-06-27 08:50:01 +0900

    Setting env

--- src/util/constants/app.tsx
+++ src/util/constants/app.tsx
@@ -1,4 +1,4 @@
-export const rootUrl = 'http://localhost:4000'; //init url
+export const rootUrl = process.env.ROOTURL; //init url
 // export const rootUrl = {서버 도메인 주소}
-export const redirectUrl = 'http://localhost:3000';
+export const redirectUrl = process.env.REDIRECTURL;
 // export const redirectUrl = {클라이언트 도메인 주소}
